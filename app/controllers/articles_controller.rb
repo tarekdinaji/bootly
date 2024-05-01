c006bb4ad14a517b350bc963c8baf2f82237c290
samiul-alim <s.a.samiru64@gmail.com>
2024-05-01 19:47:03 +0600

    create new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,20 +4,25 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
